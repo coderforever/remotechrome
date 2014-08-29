60c1538c04878d593c9258f2a870d8a11b8a3093
frank.shaof <shaofeng41@126.com>
2014-08-29 19:10:27 +0800

    remote

---
+++ javascripts/speechRecognition.js
@@ -0,0 +1,30 @@
+$(function(){
+	if(window.webkitSpeechRecognition){
+		var rec=new webkitSpeechRecognition();
+		//当用户停止说话时，语音识别将结束
+		rec.continuous=true;
+		//
+		rec.interimResults=true;
+		//语言
+		rec.lang="cmn-Hans-CN";
+		rec.start();
+		rec.onstart=function(){
+			console.log("start");
+		};
+		rec.onresult=function(event){
+			var result="";
+			for(var i=event.resultIndex;i<event.results.length;++i){
+				if(event.results[i].isFinal){
+					result+=event.results[i][0].transcript;
+				}
+			}
+			$("#result").text(result);
+		};
+		rec.onerror=function(event){
+			console.log(event);
+		};
+	}
+	else{
+		alert("fuck");
+	}
+});
